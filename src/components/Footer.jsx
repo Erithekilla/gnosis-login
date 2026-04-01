7c015dea353d0958308afb1ced929de5abab808d
Eris <montassier.erick2009@gmail.com>
2026-03-31 21:14:18 -0300

    Adicionado responsividade para o Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,37 @@
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import MenuIcon from '../assets/menu-burger.svg?react'
 import HomeIcon from '../assets/home.svg?react'
 
 function Footer(){
+    const [isOpen, setIsOpen] = useState(false)
     return(
-        <section class="footer">
-            <NavLink to="/" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
-                <HomeIcon width={30} height={30} />
-                <p>Rotina</p>
-            </NavLink>
-            <NavLink to="/a" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
-                <HomeIcon width={30} height={30} />
-                <p>Teste</p>
-            </NavLink>
-            <NavLink to="/b" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
-                <HomeIcon width={30} height={30} />
-                <p>Teste2</p>
-            </NavLink>
-            <NavLink to="/c" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
-                <HomeIcon width={30} height={30} />
-                <p>Mabi</p>
-            </NavLink>
-            <NavLink to="/login" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
-                <HomeIcon width={30} height={30} />
-                <p>Perfil</p>
-            </NavLink>
+        <section className={`footer ${isOpen ? 'open' : ''}`}>
+            <button className="menu_toggle" onClick={()=>setIsOpen(!isOpen)}>
+                <MenuIcon width={30} height={30}></MenuIcon>
+            </button>
+            <div className="footerItems">
+                <NavLink to="/" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
+                    <HomeIcon width={30} height={30} />
+                    <p>Rotina</p>
+                </NavLink>
+                <NavLink to="/a" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
+                    <HomeIcon width={30} height={30} />
+                    <p>Teste</p>
+                </NavLink>
+                <NavLink to="/b" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
+                    <HomeIcon width={30} height={30} />
+                    <p>Teste2</p>
+                </NavLink>
+                <NavLink to="/c" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
+                    <HomeIcon width={30} height={30} />
+                    <p>Mabi</p>
+                </NavLink>
+                <NavLink to="/login" className={({ isActive }) => isActive ? "itemFooter active" : "itemFooter"}>
+                    <HomeIcon width={30} height={30} />
+                    <p>Perfil</p>
+                </NavLink>
+            </div>
         </section>
     )
 }
